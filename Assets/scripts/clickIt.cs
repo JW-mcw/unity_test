0781a147418fce548d033e78802b725808328456
JWVDPLAS\Administrator <jw.van.der.plas@mcw.nl>
2018-06-25 15:13:28 +0200

    test

--- Assets/scripts/clickIt.cs
+++ Assets/scripts/clickIt.cs
@@ -12,7 +12,7 @@ public class clickIt : MonoBehaviour {
 
     void OnMouseDown()
     {
-        Debug.Log("clicked");
+        Debug.Log("clicked ball");
 
         // this object was clicked - do something
         anim.Play("MV");
