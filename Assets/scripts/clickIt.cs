7d6e4d3a65a62270c973cb2744b9a6f3989ae860
JWVDPLAS\Administrator <jw.van.der.plas@mcw.nl>
2018-06-25 14:36:47 +0200

    first commit
    test

---
+++ Assets/scripts/clickIt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickIt : MonoBehaviour {
+
+    private Animation anim;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animation>();
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("click");
+
+        // this object was clicked - do something
+        anim.Play("MV");
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
